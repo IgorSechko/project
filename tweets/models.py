90709c97be84dc2c9fb6b76b786ee2af85208521
IgorSechko <63442102+IgorSechko@users.noreply.github.com>
2020-09-21 12:19:05 +0300

    Tweets & Users

--- tweets/models.py
+++ tweets/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+from django.conf import settings
 import random
+
+User = settings.AUTH_USER_MODEL
 
 # Create your models here.
 class Tweet(models.Model):
     # id = models.AutoField(primary_key = True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # a user can have many tweets, a tweet has only one user
+    # cascade: owner deleted - all the tweets also deleted
+    # user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     content = models.TextField(blank=True,null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
 
